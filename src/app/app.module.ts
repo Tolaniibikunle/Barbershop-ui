7528979b36dc43f6202d7197c8e56e4a3981abe3
D. Ries <daniel.ries@ymail.com>
2018-09-23 21:59:31 -0400

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewsCardComponent } from './components/news-card/news-card.component';
+import { PhotoCarouselComponent } from './components/photo-carousel/photo-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NewsCardComponent,
+    PhotoCarouselComponent
   ],
   imports: [
     BrowserModule
